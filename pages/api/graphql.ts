5b0eecf8b06fd47a9fb72067ae4f15755fd0001a
chemdnepr <2880110+chemdnepr@users.noreply.github.com>
2024-04-08 22:37:11 -0600

    cors-6-fix

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -11,15 +11,27 @@ const startServer = apolloServer.start();
 
 
 export default cors(async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://canadark.com"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD"
+    );
   if (req.method === 'OPTIONS') {
     res.end();
     return false;
   }
   await startServer;
-  const handler = apolloServer.createHandler({    
+  await apolloServer.createHandler({    
     path: '/api/graphql'
-  });
-  await Cors((req, res) =>handler(req, res));
+  })(req, res);
 });
 
 export const config = {
